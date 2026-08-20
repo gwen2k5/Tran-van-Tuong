d8c9379b3fca6ba119167621d235e5ae540a0e1e
Trần Văn Tường <tuongc2k5@gmail.com>
2026-08-20 09:39:32 +0700

    Add files via upload

---
+++ lab3n13t8/src/main/java/com/mycompany/lab3n13t8/Lab3n13t8.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.lab3n13t8;
+
+/**
+ *
+ * @author tranv
+ */
+public class Lab3n13t8 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
