0eca728cb61ea43412cf863f2da0d3848855e840
Trần Văn Tường <tuongc2k5@gmail.com>
2026-08-20 09:41:37 +0700

    Add files via upload

---
+++ lab4n15t8/src/main/java/com/mycompany/lab4n15t8/Lab4n15t8.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.lab4n15t8;
+
+/**
+ *
+ * @author tranv
+ */
+public class Lab4n15t8 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
